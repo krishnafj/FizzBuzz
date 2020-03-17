48c51f3d8a41b6ec0ba8918b82b2690781d99605
Krishna Kumar <krishnafj@gmail.com>
2020-03-17 22:56:24 +1000

    Add project files.

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int intFrom = 1;
+            int intoTo = 100;
+            const string constFizz = "Fizz";
+            const string constBuzz = "Buzz";
+
+
+            //Loop from 1 - 100
+            for (int i = intFrom; i <= intoTo; i++)
+            {
+                if (i % 3 == 0 && i % 5 == 0)
+                    Console.WriteLine($"{constFizz}{constBuzz}");
+                else if(i % 3 == 0)
+                    Console.WriteLine(constFizz);
+                else if(i % 5 == 0)
+                    Console.WriteLine(constBuzz);
+                else
+                    Console.WriteLine(i);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
